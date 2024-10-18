901f6652ad3c4b18d75c2bd51a968e17ce01b90b
toucheres <761844639@qq.com>
2024-10-18 15:13:18 +0800

    更新

--- 数据结构与算法学习/练习题3.c
+++ 数据结构与算法学习/练习题3.c
@@ -2,11 +2,101 @@
 
  // Definition for singly-linked list.
 #include<stdio.h>
+#include<stdlib.h>
+
+
 struct ListNode {
 	int val;
 	struct ListNode* next;
 };
+//废案（重新生成链表，不知为何当k很大时超时）
+//struct ListNode* rotateRight(struct ListNode* head, int k)
+//{
+//	if (head == NULL)
+//	{
+//		return head;
+//	}
+//	if (head->next == NULL)
+//	{
+//		return head;
+//	}
+//	if (k == 0)
+//	{
+//		return head;
+//	}
+//
+//	struct ListNode* oldend = head;
+//	int leng = 1;
+//	while (oldend->next != NULL)
+//	{
+//		oldend = oldend->next;
+//		leng++;
+//	}
+//
+//	k = k % leng;
+//
+//	if (leng == 2)
+//	{
+//		if (k % 2 == 0)
+//		{
+//			return head;
+//		}
+//		struct ListNode* tp = head->next;
+//		head->next->next = head;
+//		head->next = NULL;
+//		return tp;
+//	}
+//
+//	struct ListNode* start = head->next;
+//	if (start == NULL)
+//	{
+//		start = head;
+//	}
+//	struct ListNode* end = head;
+//
+//
+//	for (int i = 0; i < k; i++)
+//	{
+//		end = end->next;
+//		if (end == NULL)
+//		{
+//			end = head;
+//		}
+//	}
+//	start = end->next;
+//	if (start == NULL)
+//	{
+//		start = head;
+//	}
+//	end->next = NULL;
+//	oldend->next = head;
+//	return start;
+//}
 
+//给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。
+
+// 
+//
+//示例 1：
+//
+//
+//输入：head = [1, 2, 3, 4, 5], k = 2
+//输出：[4, 5, 1, 2, 3]
+//示例 2：
+//
+//
+//输入：head = [0, 1, 2], k = 4
+//输出：[2, 0, 1]
+//
+//
+//提示：
+//
+//链表中节点的数目在范围[0, 500] 内
+//- 100 <= Node.val <= 100
+//0 <= k <= 2 * 109
+
+
+//还是只通过了一半
 struct ListNode* rotateRight(struct ListNode* head, int k)
 {
 	if (head == NULL)
@@ -17,112 +107,46 @@ struct ListNode* rotateRight(struct ListNode* head, int k)
 	{
 		return head;
 	}
+	if (head->next->next == NULL)
+	{
+		return head;
+	}
 	if (k == 0)
 	{
 		return head;
 	}
 
-	struct ListNode* oldend = head;
-	int leng = 1;
-	while (oldend->next != NULL)
+	int num = 0;
+	struct ListNode* mmm = head->next;
+	struct ListNode* tail = head;
+	while (mmm != NULL)
 	{
-		oldend = oldend->next;
-		leng++;
+		tail = mmm;
+		mmm = mmm->next;
+		num++;
 	}
+	tail->next = head;
+	k = (k % num);
+	k = num - k;
 
-	k = k % leng;
 
-	if (leng == 2)
-	{
-		if (k % 2 == 0)
-		{
-			return head;
-		}
-		struct ListNode* tp = head->next;
-		head->next->next = head;
-		head->next = NULL;
-		return tp;
-	}
 
-	struct ListNode* start = head->next;
-	if (start == NULL)
-	{
-		start = head;
-	}
-	struct ListNode* end = head;
-
+	struct ListNode* tphead = head->next;
+	struct ListNode* tpheadlast = head;
 
 	for (int i = 0; i < k; i++)
 	{
-		end = end->next;
-		if (end == NULL)
-		{
-			end = head;
-		}
+		tpheadlast = tphead;
+		tphead = tphead->next;
 	}
-	start = end->next;
-	if (start == NULL)
-	{
-		start = head;
-	}
-	end->next = NULL;
-	oldend->next = head;
-	return start;
+	tpheadlast->next = NULL;
+	//head->next = tphead ;
+	return tphead;
 }
 
-
-struct ListNode* rotateRight1(struct ListNode* head, int k)
-{
-	if (head == NULL || head->next == NULL || k == 0)
-	{
-		return head;
-	}
-
-	struct ListNode* oldend = head;
-	int length = 1;
-
-	// 计算链表长度并找到尾节点
-	while (oldend->next != NULL)
-	{
-		oldend = oldend->next;
-		length++;
-	}
-
-	// 计算实际需要旋转的步数
-	k = k % length;
-	if (k == 0)
-	{
-		return head; // 如果旋转的步数等于链表长度，则不变
-	}
-
-	// 找到新的断点
-	struct ListNode* newend = head;
-	for (int i = 1; i < length - k; i++)  // 找到新尾节点
-	{
-		newend = newend->next;
-	}
-
-	struct ListNode* newhead = newend->next; // 新的头节点
-	newend->next = NULL; // 断开旧链表
-	oldend->next = head; // 连接尾部到原头
-
-	return newhead; // 返回新的头节点
-}
-
-
+ 
 int main()
 {
 
 }
 
-//执行用时分布
-//0
-//ms
-//击败
-//100.00 %
-//复杂度分析
-//消耗内存分布
-//10.43
-//MB
-//击败
-//44.39 %
